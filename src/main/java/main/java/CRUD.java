bacf2d978bb2f8700e2afcfe3238abc538254035
Roman <rbay@bk.ru>
2016-10-18 11:37:10 +0300

    23

--- src/main/java/main/java/CRUD.java
+++ src/main/java/main/java/CRUD.java
@@ -12,13 +12,14 @@ public class CRUD {
 
 
         Client client5 = new Client(
-                "Ivan",
-                "Boiko",
-                new Date( 85,5,25 ),
-                "0904 080905",
-                "Moscow ....",
-                "ivan_boiko@mail.ru",
-                "qwerty" );
+                //"Ivan",
+                //"Boiko",
+                //new Date( 85,5,25 ),
+                //"0904 080905",
+                //"Moscow ....",
+                //"ivan_boiko@mail.ru",
+                //"qwerty"
+        );
 
         // проверка геттера
         System.out.println( "\n " + client5.getAddress() );
@@ -31,21 +32,24 @@ public class CRUD {
         System.out.println( client5.toString() );
 
         Tariff tariff2 = new Tariff(
-                "Optimum",
-                350.00);
+                //"Optimum",
+                //350.00
+        );
         System.out.println( tariff2.toString() );
 
         Contract contract2 = new Contract(
-                "+7(909)450-30-50",
-                2,
-                1,
-                0);
+                //"+7(909)450-30-50",
+                //2,
+                //1,
+                //0
+        );
         System.out.println( contract2.toString() );
 
         Option option3 = new Option(
-                "10Gb internet",
-                50.00,
-                100.00);
+                //"10Gb internet",
+                //50.00,
+                //100.00
+        );
         System.out.println( option3.toString() );
     }
 }
--- src/main/java/main/java/ClientDaoTest.java
+++ src/main/java/main/java/ClientDaoTest.java
@@ -2,74 +2,77 @@ package main.java;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 public class ClientDaoTest {
 
-    ClientDao service = new ClientDao();
+    ClientDao clientDao = new ClientDao();
 
     @Test
     public void testAdd() throws Exception {
 
         //Создаем клиента для записи в БД
-        Client client1 = new Client(
-                "Ivan",
-                "Boiko",
-                new Date( 85, 5, 25 ),
-                "0904 080905",
-                "Moscow ....",
-                "ivan_boiko@mail.ru",
-                "qwerty");
+        Client client1 = new Client();
+        client1.setFirstName( "Dima" );
+        client1.setLastName( "Kotov" );
+        client1.setBirthDay( new Date( 89, 1, 13 ));
+        client1.setPassNo( "0405 215487" );
+        client1.setAddress( "Lugi..." );
+        client1.setEmail( "d.kotov@ya.ru" );
+        client1.setPassWord( "qazqaz" );
 
         //Записали в БД
-        service.add( client1 );
+        clientDao.add( client1 );
     }
 
     @Test
     public void testGet() throws Exception {
 
         // создаем клиента для записи в БД
-        Client client2 = new Client(
-                "Petr",
-                "Semko",
-                new Date( 87, 11, 15 ),
-                "0987 245907",
-                "Spb.....",
-                "petr_semko@mail.ru",
-                "asdasd");
+        Client client2 = new Client();
+                //"Petr",
+                //"Semko",
+                //new Date( 87, 11, 15 ),
+                //"0987 245907",
+                //"Spb.....",
+                //"petr_semko@mail.ru",
+                //"asdasd"
+
 
         //Записываем в БД
-        service.add( client2 );
+        clientDao.add( client2 );
 
         //получние Петра с БД
-        Client clientFromDB = service.get( client2.getId() );
+        Client clientFromDB = clientDao.get( client2.getId() );
         System.out.println( clientFromDB );
     }
 
     @Test
     public void testUpdate() throws Exception {
         //Создаем клиента для записи в БД
-        Client client3 = new Client(
-                "Vern",
-                "Jambo",
-                new Date( 73, 1, 13 ),
-                "1745 473498",
-                "Kiev.....",
-                "vern_jambo@mail.ru",
-                "123123");
+        Client client3 = new Client();
+                //"Vern",
+                //"Jambo",
+                //new Date( 73, 1, 13 ),
+                //"1745 473498",
+                //"Kiev.....",
+                //"vern_jambo@mail.ru",
+                //"123123"
+
 
         //Записываем в БД
-        service.add( client3 );
+        clientDao.add( client3 );
 
         // Верн поменял паспорт
         client3.setPassNo( "0000 112233" );
 
         //Обновляем
-        service.update( client3 );
+        clientDao.update( client3 );
 
         //Получаем обновленую запись
-        Client updatedClient = service.get( client3.getId() );
+        Client updatedClient = clientDao.get( client3.getId() );
         System.out.println( updatedClient );
     }
 
@@ -77,7 +80,7 @@ public class ClientDaoTest {
     public void testGetAll() throws Exception {
 
         //Получаем всех клиентов с БД
-        List<Client> clients = service.getAll();
+        List<Client> clients = clientDao.getAll();
 
         //Выводим полученый список клиентов
         for( Client c : clients ){
@@ -89,6 +92,6 @@ public class ClientDaoTest {
     public void testDelete() throws Exception {
 
         //Удалем первую запись в БД
-        service.delete( 1 );
+        clientDao.delete( 1 );
     }
 }
